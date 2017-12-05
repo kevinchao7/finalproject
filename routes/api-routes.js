2866a993dadfc27a020ae772efa5ec7cb24cb31a
kevin chao <kchao562@gmail.com>
2017-12-04 20:03:54 -0800

    update

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,8 +11,11 @@ module.exports = (app,db,passport)=>{
   require('./client_route.js')(app,db);
 
   app.get('/auth/google', passport.authenticate('google', { scope : ['profile','email'] } ) );
-  app.get('/auth/google/callback', passport.authenticate('google',{ successRedirect : '/dashboard',  failureRedirect : '/'})
-  );
+  app.get('/auth/google/callback', passport.authenticate('google',{failureRedirect : '/'}),function(req,res){
+    if(req.user || req.session.user){
+      return res.redirect('/dashboard');
+    }
+  });
 
   // app.get('/auth/google', passport.authenticate('google', { scope : ['profile','email'] } ) );
   // app.get('/auth/google/callback', passport.authenticate('google',{ successRedirect : '/#!/app/dashboard', failureRedirect : '/#!/page/login'} )
