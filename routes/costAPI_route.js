1d429e3fe88481e058ad79b32df0f5abb9ea79db
kevin chao <kchao562@gmail.com>
2017-12-05 21:02:41 -0800

    update

--- routes/costAPI_route.js
+++ routes/costAPI_route.js
@@ -64,46 +64,45 @@ module.exports = (app,db)=> {return {
 
 
 // Updates current month's spending
-function dbTrigger(db,req){
-  db.clients.findOne( { where: { id : 2 } }).then((clientResp)=>{
-    db.flexspend.findAll( { where: { clientid : req.user,
-      createdAt : { $gte: moment().subtract(1, 'months').toDate() }
-    } }).then((flexResp)=>{
-      db.goals.findAll( { where : { clientid : req.user }
-      }).then((goalsResp)=>{
-        db.fixedcosts.findAll( { where : { clientid : req.user }
-        }).then((fixResp)=>{
-          console.log('sucesss!!! user=>'+ JSON.stringify(clientResp,null,4) +', flexspend =>' + JSON.stringify(flexResp,null,4) + ', goalsResp =>' + JSON.stringify(goalsResp,null,4) + ', fixResp =>' + JSON.stringify(fixResp,null,4));
-
-          if(true){ // Capability of sending email
-            // Send email if monthly expenses are over 90% of income.
-            var month_expense = 0.00;
-            fixResp.forEach((value)=>{
-              month_expense += parseFloat(value.cost);
-            });
-            flexResp.forEach((value)=>{
-              month_expense += parseFloat(value.cost);
-            });
-            goalsResp.forEach((value)=>{
-              month_expense += parseFloat(value.cost);
-            });
-
-            console.log('Month Expense: $' + month_expense);
-            // Send warning email that monthly expense is near exceeding monthly income.
-            if( month_expense  >= parseFloat(clientResp.monthly_income) * parseFloat(clientResp.reminder)){
-              const subject = 'Friendly Reminder: Your current month\'s expenses is almost exhausting your entire month\'s income.'
-              var message = '<p>Hi ' + clientResp.client_name + ',</p><p>\tSlow down your spending this month. You are nearly exhausting your entire monthly income you earned this month. Your monthly expenses is currently at $' + month_expense + ' while your current monthly income is $' + clientResp.monthly_income + '.</p><p>Thank you for using our app. </p><p>Sincerely,<br />Saving Salmon</p>';
-              emailAPI.send(subject,message,clientResp.email);
-            }
-
-          }
-
-        });
-      });
-    });
-  });
-
-}
+// function dbTrigger(db,req){
+//   db.clients.findOne( { where: { id : req.user } }).then((clientResp)=>{
+//     db.flexspend.findAll( { where: { clientid : req.user,
+//       createdAt : { $gte: moment().subtract(1, 'months').toDate() }
+//     } }).then((flexResp)=>{
+//       db.goals.findAll( { where : { clientid : req.user }
+//       }).then((goalsResp)=>{
+//         db.fixedcosts.findAll( { where : { clientid : req.user }
+//         }).then((fixResp)=>{
+//           console.log('sucesss!!! user=>'+ JSON.stringify(clientResp,null,4) +', flexspend =>' + JSON.stringify(flexResp,null,4) + ', goalsResp =>' + JSON.stringify(goalsResp,null,4) + ', fixResp =>' + JSON.stringify(fixResp,null,4));
+//
+//           if(true){ // Capability of sending email
+//             // Send email if monthly expenses are over 90% of income.
+//             var month_expense = 0.00;
+//             fixResp.forEach((value)=>{
+//               month_expense += parseFloat(value.cost);
+//             });
+//             flexResp.forEach((value)=>{
+//               month_expense += parseFloat(value.cost);
+//             });
+//             goalsResp.forEach((value)=>{
+//               month_expense += parseFloat(value.cost);
+//             });
+//
+//             console.log('Month Expense: $' + month_expense);
+//             // Send warning email that monthly expense is near exceeding monthly income.
+//             if( month_expense  >= parseFloat(clientResp.monthly_income) * parseFloat(clientResp.reminder)){
+//               const subject = 'Friendly Reminder: Your current month\'s expenses is almost exhausting your entire month\'s income.'
+//               var message = '<p>Hi ' + clientResp.client_name + ',</p><p>\tSlow down your spending this month. You are nearly exhausting your entire monthly income you earned this month. Your monthly expenses is currently at $' + month_expense + ' while your current monthly income is $' + clientResp.monthly_income + '.</p><p>Thank you for using our app. </p><p>Sincerely,<br />Saving Salmon</p>';
+//               emailAPI.send(subject,message,clientResp.email);
+//             }
+//
+//           }
+//
+//         });
+//       });
+//     });
+//   });
+// }
 
 
 function ErrorMessage(res){
