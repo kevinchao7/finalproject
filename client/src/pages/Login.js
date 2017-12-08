8eb828a5bfcdaa1db0fdd80ccbd2751b692b93f4
kevin chao <kchao562@gmail.com>
2017-12-07 19:09:29 -0800

    update

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -15,7 +15,7 @@ const About = () =>
       <Row>
         <Col size="md-12">
           <p>Please Log In.</p>
-          <a href="http://fbaproject.herokuapp.com/auth/google" class="btn btn-danger"><span class="fa fa-google-plus"></span> Google</a>
+          <a href="auth/google" class="btn btn-danger"><span class="fa fa-google-plus"></span> Google</a>
         </Col>
       </Row>
     </Container>
