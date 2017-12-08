b0dd96ee289f76b5f7847f5bdf6ef614d8d74efd
kevin chao <kchao562@gmail.com>
2017-12-07 19:29:09 -0800

    update

--- config/keys.js
+++ config/keys.js
@@ -2,10 +2,10 @@ const randomString = require('randomstring');
 module.exports = {
   google : {
     clientID: '393852577124-du4hrae8odanu37sh8cgacmhk3hmolvg.apps.googleusercontent.com',
-    clientSecret: 'n5jR3BfnGWOmsU4U0w-bno5S',
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "https://fbaproject.herokuapp.com/auth/google/callback"
     // callbackURL: "http://localhost:3001/auth/google/callback"
   },
-  SENDGRID_API_KEY : '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',
+  SENDGRID_API_KEY : process.env.SENDGRID_API_KEY,
   session_key : randomString.generate()
 }
